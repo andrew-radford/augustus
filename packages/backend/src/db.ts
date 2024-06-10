b0ca2d1216b8e932313ded5932fcd43f7b6f5bc8
Alex Burns <aburnzie@gmail.com>
2024-06-09 19:54:13 -0600

    Add config comments and prettify

--- packages/backend/src/db.ts
+++ packages/backend/src/db.ts
@@ -1,12 +1,13 @@
 import mysql from "mysql2/promise";
+// If the below line is failing to import, please follow the instructions in ./default_config.ts
 import { BackendConfig } from "./config";
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: "localhost",
-  user: BackendConfig.DB_USER,
-  password: BackendConfig.DB_PASSWORD,
-  database: "user_service", // Replace with your database name
+    connectionLimit: 10,
+    host: "localhost",
+    user: BackendConfig.DB_USER,
+    password: BackendConfig.DB_PASSWORD,
+    database: "user_service", // Replace with your database name
 });
 
 export default pool;
--- packages/backend/src/default_config.ts
+++ packages/backend/src/default_config.ts
@@ -1,4 +1,4 @@
-// Copy this file and name it config.ts
+// Copy this file and name it config.ts (/packages/backend/src/config.ts)
 export const BackendConfig = {
     DB_USER: 'root',
     DB_PASSWORD: ''
